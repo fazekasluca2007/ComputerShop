e0d8da40c2d9f2a71a4d508b11e7a7cc55fd8bcb
fazekasluca2007 <fazekasl@kkszki.hu>
2025-09-29 10:43:39 +0200

    Connect.cs  is ready to use

---
+++ ComputerShop/Connect.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShop
+{
+    internal class Connect
+    {
+        public MySqlConnection _connection;
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _pass;
+
+        private string _connectionString;
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "computershop";
+            _user = "root";
+            _pass = "";
+
+            _connectionString = $"SERVER={_host};DATABASE={_db};UID={_user};PASSWORD={_pass};SslMode=None";  
+
+            _connection= new MySqlConnection(_connectionString);
+        }
+    }
+}
